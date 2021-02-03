1b778c4dd87d0b75c0678ee849d8c44a964e14d4
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-02-03 23:39:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { ToastContainer } from 'react-toastify';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import 'react-toastify/dist/ReactToastify.css';
-
 import Form from './Components/Form/Form';
 import ContactList from './Components/ContactList/ContactList';
 import Filter from './Components/Filter/Filter';
 import Title from './Components/Title/Title';
 import Container from './Components/Container/Container';
+import contactsOperations from './redux/contacts/contacts-operations';
 
 export default function App() {
+  const dispatch = useDispatch();
+  const fetchContacts = () => dispatch(contactsOperations.fetchContacts());
+
+  useEffect(() => {
+    fetchContacts();
+  });
+
   return (
     <Container>
       <ToastContainer autoClose={3000} />
