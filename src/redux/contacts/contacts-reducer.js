bab222936768ccb1f2e971e96cf94be4b7d03050
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-02-03 23:39:47 +0200

    Update contacts-reducer.js

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -8,9 +8,13 @@ import {
   deleteContactSuccess,
   deleteContactError,
   changeFilter,
+  fetchContactRequest,
+  fetchContactSuccess,
+  fetchContactError,
 } from '../contacts/contacts-actions';
 
 const items = createReducer([], {
+  [fetchContactSuccess]: (_, { payload }) => payload,
   [addContactSuccess]: (state, { payload }) => [...state, payload],
   [deleteContactSuccess]: (state, { payload }) =>
     state.filter(({ id }) => id !== payload),
@@ -21,6 +25,10 @@ const filter = createReducer('', {
 });
 
 const loading = createReducer(false, {
+  [fetchContactRequest]: () => true,
+  [fetchContactSuccess]: () => false,
+  [fetchContactError]: () => false,
+
   [addContactRequest]: () => true,
   [addContactSuccess]: () => false,
   [addContactError]: () => false,
