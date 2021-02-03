c67f4a871b2fbb1d830de05cba2d10b18cc24c6f
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-02-03 13:59:27 +0200

    Create Filter.js

---
+++ src/Components/Filter/Filter.js
@@ -0,0 +1,20 @@
+import { useSelector, useDispatch } from 'react-redux';
+import contactActions from '../../redux/contacts/contacts-actions';
+import { getFilter } from '../../redux/contacts/contacts-selectors';
+
+import s from './Filter.module.css';
+
+export default function Filter() {
+  const value = useSelector(getFilter);
+  const dispatch = useDispatch();
+  return (
+    <label className={s.label}>
+      Find contacts by name
+      <input
+        type="text"
+        value={value}
+        onChange={e => dispatch(contactActions.changeFilter(e.target.value))}
+      ></input>
+    </label>
+  );
+}
