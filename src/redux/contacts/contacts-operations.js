c233f1328f4703d8790c1a89d4e08b27c15cbdc0
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-02-03 23:05:18 +0200

    Create contacts-operations.js

---
+++ src/redux/contacts/contacts-operations.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import {
+  addContactRequest,
+  addContactSuccess,
+  addContactError,
+  deleteContactRequest,
+  deleteContactSuccess,
+  deleteContactError,
+} from './contacts-actions';
+
+axios.defaults.baseURL = 'http://localhost:4040';
+
+const addContact = (name, number) => dispatch => {
+  const contact = {
+    name,
+    number,
+  };
+
+  dispatch(addContactRequest());
+
+  axios
+    .post('/contacts', contact)
+    .then(({ data }) => dispatch(addContactSuccess(data)))
+    .catch(error => dispatch(addContactError(error)));
+};
+
+const deleteContact = contactId => dispatch => {
+  dispatch(deleteContactRequest());
+
+  axios
+    .delete(`contacts/${contactId}`)
+    .then(() => dispatch(deleteContactSuccess(contactId)))
+    .catch(error => dispatch(deleteContactError(error)));
+};
+
+export default {
+  addContact,
+  deleteContact,
+};
