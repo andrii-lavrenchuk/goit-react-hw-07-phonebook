24021d352da18aeaeac65acb329f8d371614cad8
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-02-04 18:22:01 +0200

    Update contacts-operations.js

--- src/redux/contacts/contacts-operations.js
+++ src/redux/contacts/contacts-operations.js
@@ -13,13 +13,21 @@ import {
 
 axios.defaults.baseURL = 'http://localhost:4040';
 
-const fetchContacts = () => dispatch => {
+const fetchContacts = () => async dispatch => {
   dispatch(fetchContactRequest());
 
-  axios
-    .get('/contacts')
-    .then(({ data }) => dispatch(fetchContactSuccess(data)))
-    .catch(error => dispatch(fetchContactError(error)));
+  try {
+    const { data } = await axios.get('/contacts');
+
+    dispatch(fetchContactSuccess(data));
+  } catch (error) {
+    dispatch(fetchContactError(error));
+  }
+
+  //   axios
+  //     .get('/contacts')
+  //     .then(({ data }) => dispatch(fetchContactSuccess(data)))
+  //     .catch(error => dispatch(fetchContactError(error)));
 };
 
 const addContact = (name, number) => dispatch => {
